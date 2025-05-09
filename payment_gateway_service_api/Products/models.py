5d82f0e9da4a7bdb3fd777d2c62391c293ac9aad
Igboke Daniel <danieligboke669@gmail.com>
2025-05-09 21:51:38 +0100

    Added products model

--- payment_gateway_service_api/Products/models.py
+++ payment_gateway_service_api/Products/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+class Products(models.Model):
+    """
+    This is the model for the products
+    """
+    name = models.CharField(max_length=255, unique=True, help_text="Enter Product Name")
+    quantity = models.IntegerField(help_text="Enter Product Quantity")
+    description = models.TextField(help_text="Enter Product Description")
+    price = models.DecimalField(max_digits=10, decimal_places=2, help_text="Enter Product Price")
+    created_at = models.DateTimeField(auto_now_add=True, help_text="Enter Date Created")
+    updated_at = models.DateTimeField(auto_now=True, help_text="Enter Date Updated")
+    is_available = models.BooleanField(default=True, help_text="Is Product Available")
+
+    def __str__(self):
+        return self.name
+    class Meta:
+        verbose_name = "Product"
+        verbose_name_plural = "Products"
+        ordering = ["-created_at"]
