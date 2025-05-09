cf248288bfde0764bf8dd0219c6cc9aaa7460624
Igboke Daniel <danieligboke669@gmail.com>
2025-05-09 22:23:12 +0100

    Add validation to quantity field in Products model to ensure non-negative values

--- payment_gateway_service_api/Products/models.py
+++ payment_gateway_service_api/Products/models.py
@@ -1,11 +1,13 @@
 from django.db import models
+from django.db.models import Q, CheckConstraint
+from django.core.validators import MinValueValidator
 
 class Products(models.Model):
     """
     This is the model for the products
     """
     name = models.CharField(max_length=255, unique=True, help_text="Enter Product Name")
-    quantity = models.IntegerField(help_text="Enter Product Quantity")
+    quantity = models.IntegerField(help_text="Enter Product Quantity",validators=[MinValueValidator(0)])
     description = models.TextField(help_text="Enter Product Description")
     price = models.DecimalField(max_digits=10, decimal_places=2, help_text="Enter Product Price")
     created_at = models.DateTimeField(auto_now_add=True, help_text="Enter Date Created")
@@ -18,3 +20,9 @@ class Products(models.Model):
         verbose_name = "Product"
         verbose_name_plural = "Products"
         ordering = ["-created_at"]
+        constraints = [
+            CheckConstraint(
+                check=Q(quantity__gte=0),
+                name="quantity_greater_than_zero",
+            ),
+        ]
