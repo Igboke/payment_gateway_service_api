782bcf25bb387c1d885ae9ffe487b79fc35cf07e
Igboke Daniel <danieligboke669@gmail.com>
2025-05-09 21:01:46 +0100

    Add ClientAdmin class to manage Client model in admin interface

--- payment_gateway_service_api/clients/admin.py
+++ payment_gateway_service_api/clients/admin.py
@@ -1,3 +1,52 @@
 from django.contrib import admin
+from .models import Client
+from django.contrib.auth.admin import UserAdmin
+from .forms import ClientCreationForm, ClientChangeForm
+from django.contrib.auth import get_user_model
 
-# Register your models here.
+class ClientAdmin(UserAdmin):
+    """
+    This is the admin class for Client
+    """
+    form = ClientChangeForm
+    add_form = ClientCreationForm
+    list_display = ("email","first_name","last_name","is_staff","is_active")
+    list_filter = ("is_staff","is_active")
+    search_fields = ("email","first_name","last_name","middle_name")
+    ordering = ("email",)
+    filter_horizontal = ()
+    add_fieldsets = (
+        (None, {
+            "classes":("wide","collapse"),
+            "fields":("email","password","password2"),    
+        }),
+        ("Personal Info", {
+            "classes":("collapse",),
+            "fields":("first_name","middle_name","last_name")
+        }),
+        ("Address", {
+            "classes":("collapse",),
+            "fields":("street","town","state")
+        }),
+    )
+    fieldsets = (
+        (None, {
+            "fields":("email",),
+        }),
+        ("Personal Info", {
+            "classes":("collapse",),
+            "fields":("first_name","middle_name","last_name")
+        }),
+        ("Address", {
+            "classes":("collapse",),
+            "fields":("street","town","state")
+        }),
+        ("Permissions", {
+            "fields":("is_staff","is_active"),
+        }),
+        ("Important Dates", {
+            "fields":("last_login","date_joined"),
+        }),
+    )
+
+admin.site.register(Client, ClientAdmin)
