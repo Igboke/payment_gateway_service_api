0f2bb9b25b68abdf7de0073c42855b87984d4f96
Igboke Daniel <danieligboke669@gmail.com>
2025-05-10 01:08:53 +0100

    Refactor Orders model to use ForeignKey for shipping and billing addresses, add OrderItem model, and implement total amount calculation

--- payment_gateway_service_api/Orders/models.py
+++ payment_gateway_service_api/Orders/models.py
@@ -1,5 +1,8 @@
 from django.db import models
+from django.core.validators import MinValueValidator
 from django.contrib.auth import get_user_model
+from Products.models import Products
+from clients.utils import Address
 
 ClientModel = get_user_model()
 
@@ -9,11 +12,11 @@ STATUS_CHOICES=[
     ("failed",("Failed"))
 ]
 class Orders(models.Model):
-    client = models.ForeignKey(ClientModel,on_delete=models.CASCADE,related_name="orders",help_text="Select the Client making the Order")
+    client = models.ForeignKey(ClientModel,on_delete=models.DO_NOTHING,related_name="orders",help_text="Select the Client making the Order")
     status = models.CharField(max_length=15,default="pending",choices=STATUS_CHOICES,help_text="Status Choices")
-    total_amount = models.DecimalField(max_digits=10, decimal_places=2, help_text="Order Total",null=True)
-    shipping_address = models.CharField(max_length=100,help_text="Shipping Adress")
-    billing_address = models.CharField(max_length=100,help_text="Billing Adress")
+    total_amount = models.DecimalField(max_digits=10, decimal_places=2, help_text="Order Total",null=False,blank=True)
+    shipping_address = models.ForeignKey(Address,on_delete=models.DO_NOTHING,related_name="shipped_orders",help_text="Shipping Adress")
+    billing_address = models.ForeignKey(Address,on_delete=models.DO_NOTHING,help_text="Billing Adress",related_name="billed_orders")
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
@@ -22,8 +25,29 @@ class Orders(models.Model):
         verbose_name_plural = "Orders"
         ordering = ["-created_at"]
 
+    def calculate_total_amount(self):
+        total = sum(item.product.price * item.quantity for item in self.order_line.all())
+        self.total_amount = total
+    
+    def save(self, *args, **kwargs):
+        if self.pk and not self.total_amount: 
+            self.calculate_total_amount()
+        super().save(*args, **kwargs)
+
     def __str__(self):
-        return f"{self.client} -- #Order No: {Orders.pk}"
+        return f"{self.client} -- #Order No: {self.pk}"
+    
+class OrderItem(models.Model):
+    products = models.ForeignKey(Products,on_delete=models.DO_NOTHING,related_name="product_items")
+    orders = models.ForeignKey(Orders,on_delete=models.CASCADE,related_name="order_line")
+    quantity = models.IntegerField(help_text="Enter OrderItem Quantity",validators=[MinValueValidator(1)])
+
+    class Meta:
+        verbose_name = "Order Item"
+        verbose_name_plural = "Order Items"
+
+    def __str__(self):
+        return f"{self.pk}"
     
 
 
