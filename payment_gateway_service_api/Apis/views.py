35254277107cffb2c7cfaec08034de46e19d82bf
Igboke Daniel <danieligboke669@gmail.com>
2025-05-11 12:05:46 +0100

    Add InitiatePaymentView API for handling payment requests

--- payment_gateway_service_api/Apis/views.py
+++ payment_gateway_service_api/Apis/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.views import APIView
 
-# Create your views here.
+class InitiatePaymentView(APIView):
+    """
+    This is an Initiate payment API function, simply for requests and response
+    """
+    def post(self,request,*args,**kwargs):
+        #pass request data to serializer for validaton and conversion to Python data types
+
+        pass
+
