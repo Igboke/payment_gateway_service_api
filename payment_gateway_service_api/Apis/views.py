717ef98c419d26209c813d603080f385bccab8a6
Igboke Daniel <danieligboke669@gmail.com>
2025-05-11 22:44:25 +0100

    Add validation for BankTransferSerializer in InitiatePaymentView

--- payment_gateway_service_api/Apis/views.py
+++ payment_gateway_service_api/Apis/views.py
@@ -1,6 +1,7 @@
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.views import APIView
+from .serializers import BankTransferSerializers
 
 class InitiatePaymentView(APIView):
     """
@@ -8,6 +9,14 @@ class InitiatePaymentView(APIView):
     """
     def post(self,request,*args,**kwargs):
         #pass request data to serializer for validaton and conversion to Python data types
+        serializer = BankTransferSerializers(data=request.data)
+        
+
+        if not serializer.is_valid():
+            return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+        
+        
+
 
         pass
 
