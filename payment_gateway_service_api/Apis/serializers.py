c3e45ab4884920c57918949219cc6e63336169bc
Igboke Daniel <danieligboke669@gmail.com>
2025-05-11 12:14:24 +0100

    Add serializers for Address and update Client serializer to include house_address

--- payment_gateway_service_api/Apis/serializers.py
+++ payment_gateway_service_api/Apis/serializers.py
@@ -2,10 +2,40 @@ from rest_framework import serializers
 from Products.models import Products
 from Orders.models import Orders, OrderItem
 from django.contrib.auth import get_user_model
+from clients.utils import Address
 
 ClientModel = get_user_model()
 
+class AddressSerializers(serializers.ModelSerializer):
+    """
+    Address Serializer
+    """
+    class Meta:
+        model = Address
+        fields = [
+            "id",
+            "street_line_1",
+            "street_line_2",
+            "city",
+            "state_province",
+            "postal_code",
+            "country",
+        ]
+
 class ClientModelSerializers(serializers.ModelSerializer):
+    """
+    Client Serializer
+    """
+    house_address = AddressSerializers(many=True, required=False)
+    class Meta:
+        model = ClientModel
+        fields = [
+            "id",
+            "first_name",
+            "last_name",
+            "email",
+            "house_address",
+        ]
     pass
 
 class ProductsSerializers(serializers.ModelSerializer):
