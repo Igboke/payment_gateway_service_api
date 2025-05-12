be43fa50a96799dac8a06b034382d5b574093824
Igboke Daniel <danieligboke669@gmail.com>
2025-05-12 12:21:30 +0100

    Enhance InitiatePaymentView to validate client email and return 404 if not found

--- payment_gateway_service_api/Apis/views.py
+++ payment_gateway_service_api/Apis/views.py
@@ -2,6 +2,8 @@ from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.views import APIView
 from .serializers import BankTransferSerializers
+from utils import get_client_by_mail
+from .core_logic import PaymentDetails
 
 class InitiatePaymentView(APIView):
     """
@@ -10,12 +12,17 @@ class InitiatePaymentView(APIView):
     def post(self,request,*args,**kwargs):
         #pass request data to serializer for validaton and conversion to Python data types
         serializer = BankTransferSerializers(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        validated_data = serializer.validated_data
+
+        # Extract the validated data
+        client = get_client_by_mail(validated_data["email"])
+
+        if not client:
+            return Response({"error": "Client not found"}, status=status.HTTP_404_NOT_FOUND)
+
         
 
-        if not serializer.is_valid():
-            return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
-        
-        
 
 
         pass
