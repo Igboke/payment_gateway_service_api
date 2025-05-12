8eaf9fcc4fab5e6cab45b633e41d39a91f1e82d6
Igboke Daniel <danieligboke669@gmail.com>
2025-05-12 14:47:26 +0100

    Integrate client details retrieval and transaction setup in PaymentServiceCore

--- payment_gateway_service_api/Apis/core_logic.py
+++ payment_gateway_service_api/Apis/core_logic.py
@@ -1,5 +1,6 @@
 from dataclasses import dataclass
 from ports_and_adapters import PaymentGatewayInterface
+from utils import ClientPaymentDetails, get_client_by_mail
 
 @dataclass
 class PaymentDetails:
@@ -8,12 +9,19 @@ class PaymentDetails:
     currency: str
     client_email: str
     client_name: str
-    is_pemanent: bool = False
+    is_permanent: bool = False
 
 class PaymentServiceCore:
     def __init__(self,gateway_adapter: PaymentGatewayInterface):
         self.gateway_adapter = gateway_adapter
 
     def initiate_payment(self,payment_details: PaymentDetails) -> dict:
+        self.client = get_client_by_mail(PaymentDetails.client_email)
+        client_payment_transaction_instance = ClientPaymentDetails(self.client).set_transaction_model()
+        payment_details.tx_ref = client_payment_transaction_instance.transaction_ref
+        payment_details.amount = client_payment_transaction_instance.amount
+        payment_details.client_email = self.client.email
+        payment_details.client_name = self.client.get_full_name()
+        payment_details.currency = "NGN"
         self.gateway_adapter.process_payment(payment_details)
-        pass
+        
