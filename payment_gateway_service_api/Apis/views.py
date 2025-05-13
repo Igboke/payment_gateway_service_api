b04dbac58d82bb3e1ce7661dbf2b3c6ac3285e6c
Igboke Daniel <danieligboke669@gmail.com>
2025-05-13 18:27:35 +0100

    Refactor InitiatePaymentView to simplify payment initiation by using a dedicated service function

--- payment_gateway_service_api/Apis/views.py
+++ payment_gateway_service_api/Apis/views.py
@@ -3,10 +3,8 @@ from drf_spectacular.utils import extend_schema
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.views import APIView
-from .payments_ports_and_adapters import FlutterWaveAdapter
-from .repositories_ports_and_adapters import DjangoClientRepositoryAdapter
-from .serializers import BankTransferSerializers, BankTransferOutputSerializers 
-from .core_logic import PaymentServiceCore, InitialPaymentRequestDTO 
+from .serializers import BankTransferSerializers, BankTransferOutputSerializers  
+from .services import initiate_payment
 
 class InitiatePaymentView(APIView):
     """
@@ -23,33 +21,9 @@ class InitiatePaymentView(APIView):
         serializer.is_valid(raise_exception=True)
         validated_data = serializer.validated_data
 
-        # Instantiate the concrete adapters
-        payment_gateway_adapter = FlutterWaveAdapter()
-        client_repo_adapter = DjangoClientRepositoryAdapter()
-
-        # Instantiate the core service, injecting the adapters
-        payment_service = PaymentServiceCore(
-            gateway_adapter=payment_gateway_adapter,
-            client_repository=client_repo_adapter
-        )
-
         try:
-            # Prepare input DTO for the core service
-            initial_request_dto = InitialPaymentRequestDTO(
-                client_email=validated_data["email"],
-                currency=validated_data["currency"],
-                is_permanent=validated_data.get("is_permanent", False),
-                payment_gateway_name="FlutterWave"
-            )
-
-            # Call the core service method
-            response_dto = payment_service.initiate_payment(initial_request_dto)
-
-            # Prepare the HTTP response based on the core's output DTO
-            output_response_dict = {
-                "transaction_ref":response_dto.transaction_ref,
-                "gateway_response":response_dto.gateway_response.raw_response
-            }
+            # Call the service function to initiate payment
+            output_response_dict = initiate_payment(validated_data)
             output_serializer = BankTransferOutputSerializers(output_response_dict)
             return Response(output_serializer.data, status=status.HTTP_200_OK)
 
