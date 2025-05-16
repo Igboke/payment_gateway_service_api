c759135979d272f8a691d7fabab829014e8662db
Igboke Daniel <danieligboke669@gmail.com>
2025-05-16 12:53:37 +0100

    Add webhook URL path to urlpatterns for HandleWebhookView

--- payment_gateway_service_api/Apis/urls.py
+++ payment_gateway_service_api/Apis/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import InitiatePaymentView 
+from .views import InitiatePaymentView, HandleWebhookView 
 
 urlpatterns = [
-    path('v1/createpayment/',InitiatePaymentView.as_view(),name="create-payment")
+    path('v1/createpayment/',InitiatePaymentView.as_view(),name="create-payment"),
+    path('v1/webhook/',HandleWebhookView.as_view(),name="webhook"),
+
 ]
