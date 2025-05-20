75714c72a0e8a92ca064799e63de6c554a7d0288
Igboke Daniel <danieligboke669@gmail.com>
2025-05-20 20:16:25 +0100

    Refactor PayStackAdapter to implement webhook handling and extract relevant transaction data

--- payment_gateway_service_api/Apis/payments_ports_and_adapters.py
+++ payment_gateway_service_api/Apis/payments_ports_and_adapters.py
@@ -98,77 +98,21 @@ class PayStackAdapter(PaymentGatewayInterface):
 
     def handle_webhook(self, request_data) -> GatewayWebhookEventDTO:
         """
-        {
-        "event": "charge.success",
-        "data": {
-            "id": 302961,
-            "domain": "live",
-            "status": "success",
-            "reference": "qTPrJoy9Bx",
-            "amount": 10000,
-            "message": null,
-            "gateway_response": "Approved by Financial Institution",
-            "paid_at": "2016-09-30T21:10:19.000Z",
-            "created_at": "2016-09-30T21:09:56.000Z",
-            "channel": "card",
-            "currency": "NGN",
-            "ip_address": "41.242.49.37",
-            "metadata": 0,
-            "log": {
-            "time_spent": 16,
-            "attempts": 1,
-            "authentication": "pin",
-            "errors": 0,
-            "success": false,
-            "mobile": false,
-            "input": [],
-            "channel": null,
-            "history": [
-                {
-                "type": "input",
-                "message": "Filled these fields: card number, card expiry, card cvv",
-                "time": 15
-                },
-                {
-                "type": "action",
-                "message": "Attempted to pay",
-                "time": 15
-                },
-                {
-                "type": "auth",
-                "message": "Authentication Required: pin",
-                "time": 16
-                }
-            ]
-            },
-            "fees": null,
-            "customer": {
-            "id": 68324,
-            "first_name": "BoJack",
-            "last_name": "Horseman",
-            "email": "bojack@horseman.com",
-            "customer_code": "CUS_qo38as2hpsgk2r0",
-            "phone": null,
-            "metadata": null,
-            "risk_action": "default"
-            },
-            "authorization": {
-            "authorization_code": "AUTH_f5rnfq9p",
-            "bin": "539999",
-            "last4": "8877",
-            "exp_month": "08",
-            "exp_year": "2020",
-            "card_type": "mastercard DEBIT",
-            "bank": "Guaranty Trust Bank",
-            "country_code": "NG",
-            "brand": "mastercard",
-            "account_name": "BoJack Horseman"
-            },
-            "plan": {}
-        }
-        }
+        Handles webhook notifications from Paystack
         """
-        pass
+        # Handle webhook notifications from Paystack
+        # simply for extracting data and passing to a dto (GatewayWebhookEventDTO)
+        data = request_data.get("data", {})
+        transaction_ref = data.get("reference", "")
+        status = data.get("status", "")
+        gateway_ref = data.get("customer", "").get("customer_code", "")
+        amount = data.get("amount", 0.0)//100  # Convert to Naira
+        return GatewayWebhookEventDTO(
+            internal_transaction_ref=transaction_ref,
+            gateway_ref=gateway_ref,
+            new_status=status,
+            amount=amount
+        )
 
     def verify_payment(self, transaction_ref:str):
         pass
