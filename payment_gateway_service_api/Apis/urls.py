4c2b5fed1bdcdde90ab997c8f7b5ba29d4c9c26d
Igboke Daniel <danieligboke669@gmail.com>
2025-06-01 11:47:00 +0100

    Format URLs in payment_gateway_service_api for improved consistency and readability

--- payment_gateway_service_api/Apis/urls.py
+++ payment_gateway_service_api/Apis/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path
-from .views import InitiatePaymentView, HandleWebhookView 
+from .views import InitiatePaymentView, HandleWebhookView
 
 urlpatterns = [
-    path('v1/createpayment/',InitiatePaymentView.as_view(),name="create-payment"),
-    path('v1/webhook/',HandleWebhookView.as_view(),name="webhook"),
-
+    path("v1/createpayment/", InitiatePaymentView.as_view(), name="create-payment"),
+    path("v1/webhook/", HandleWebhookView.as_view(), name="webhook"),
 ]
