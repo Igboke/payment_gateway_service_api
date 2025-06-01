f61bac3425f35c9b228e710fb0b37f06c8c31e1a
Igboke Daniel <danieligboke669@gmail.com>
2025-06-01 11:12:41 +0100

    Enhance payment processing with detailed DTO documentation and logging

--- payment_gateway_service_api/Apis/payments_ports_and_adapters.py
+++ payment_gateway_service_api/Apis/payments_ports_and_adapters.py
@@ -4,9 +4,26 @@ from typing import Any, Dict, Optional
 from django.conf import settings
 import requests
 import paystack
+import logging
+
+logger = logging.getLogger(__name__)
+
 
 @dataclass
 class PaymentDetails:
+    """Data Transfer Object for payment details.
+    This DTO encapsulates all necessary information to process a payment.
+    - tx_ref: Unique reference for the transaction.
+    - amount: Amount to be charged.
+    - currency: Currency in which the payment is made.
+    - client_email: Email of the client making the payment.
+    - client_name: Name of the client making the payment.
+    - is_permanent: Whether the payment is for a permanent service or not.
+    - bank_code: Bank code for the payment.
+    - bank_phone: Phone number associated with the bank account.
+    - bank_token: Token for the bank account.
+    """
+
     tx_ref: str
     amount: float
     currency: str
@@ -20,27 +37,47 @@ class PaymentDetails:
 
 @dataclass
 class GatewayProcessPaymentResponseDTO:
+    """Data Transfer Object for the response from a payment gateway after processing a payment.
+    This DTO encapsulates the response data from the payment gateway after a payment attempt.
+    - success: Indicates whether the payment was successful or not.
+    - gateway_ref: Reference from the payment gateway for the transaction.
+    - raw_response: Raw response data from the payment gateway. field(default_factory=dict) ensures that each instance gets its own independent dictionary.
+    """
+
     success: bool
     gateway_ref: Optional[str] = None
     raw_response: Dict = field(default_factory=dict)
 
+
 @dataclass
 class GatewayWebhookEventDTO:
+    """Data Transfer Object for webhook events from a payment gateway.
+    This DTO encapsulates the data received from a payment gateway webhook event.
+    - internal_transaction_ref: Internal reference for the transaction.
+    - gateway_ref: Reference from the payment gateway for the transaction.
+    - new_status: New status of the transaction as reported by the payment gateway.
+    - amount: Amount associated with the transaction.
+    """
+
     internal_transaction_ref: str
     gateway_ref: str
-    new_status: str 
+    new_status: str
     amount: float
+
 
 class PaymentGatewayInterface(ABC):
     """Interface for payment gateway adapters.
     Methods in this interface should be implemented by all payment gateway adapters.
     Each adapter should handle its own specific logic for:
     - processing payments,
-    - handling webhooks, 
+    - handling webhooks,
     - verifying payments.
     """
+
     @abstractmethod
-    def process_payment(self, payment_details: PaymentDetails) -> GatewayProcessPaymentResponseDTO:
+    def process_payment(
+        self, payment_details: PaymentDetails
+    ) -> GatewayProcessPaymentResponseDTO:
         """Initiates a payment via this gateway. Returns core DTO based on gateway response."""
         pass
 
@@ -50,9 +87,10 @@ class PaymentGatewayInterface(ABC):
         pass
 
     @abstractmethod
-    def verify_payment(self, transaction_ref: str) -> Dict[str, Any]: # Or return a GatewayVerificationResponseDTO?
+    def verify_payment(self, transaction_ref: str) -> Dict[str, Any]:
         """Verifies a payment via this gateway. Returns gateway-specific response."""
         pass
+
 
 class PayStackAdapter(PaymentGatewayInterface):
     """
@@ -63,60 +101,70 @@ class PayStackAdapter(PaymentGatewayInterface):
     - Handles webhook notifications specific to flutterwave's format.
     - Verifies transaction using Paystack's verification API
     """
-    def process_payment(self, payment_details:PaymentDetails) -> GatewayProcessPaymentResponseDTO:
-        # Initialize Paystack client
+
+    def process_payment(
+        self, payment_details: PaymentDetails
+    ) -> GatewayProcessPaymentResponseDTO:
+        """
+        Processes a payment using Paystack's API.
+        This method takes payment details, constructs a request to Paystack's API,
+        and returns a DTO with the result of the payment attempt.
+        :param payment_details: PaymentDetails object containing all necessary information for the payment.
+        :return: GatewayProcessPaymentResponseDTO containing the success status, gateway reference, and raw response data.
+        """
         paystack.api_key = settings.PAYSTACK_SECRET_KEY
         amount = payment_details.amount * 100  # Paystack expects amount in kobo
         bank = {
-            "code": payment_details.bank_code,#"50211",
-            "phone": payment_details.bank_phone,#"+2348100000000",
-            "token": payment_details.bank_token,#"123456"
+            "code": payment_details.bank_code,
+            "phone": payment_details.bank_phone,
+            "token": payment_details.bank_token,
         }
- 
 
-        # Create a payment charge request
         response = paystack.Charge.create(
             email=payment_details.client_email,
             amount=amount,
             bank=bank,
             reference=payment_details.tx_ref,
-            
         )
         if response:
             status = response.status
             data = response.data
             success = status == True
-            response_data = {"data":response.data,
-                    "message":response.message,
-                    "status":response.status
+            response_data = {
+                "data": response.data,
+                "message": response.message,
+                "status": response.status,
             }
         return GatewayProcessPaymentResponseDTO(
             success=success,
             gateway_ref=data.get("reference"),
-            raw_response=response_data
+            raw_response=response_data,
         )
 
     def handle_webhook(self, request_data) -> GatewayWebhookEventDTO:
         """
         Handles webhook notifications from Paystack
+        This method processes the incoming webhook data, extracts relevant information,
+        and returns a DTO with the transaction details.
+        :param request_data: Raw data from the webhook notification.
+        :return: GatewayWebhookEventDTO containing the internal transaction reference, gateway reference, new status, and amount.
         """
-        # Handle webhook notifications from Paystack
-        # simply for extracting data and passing to a dto (GatewayWebhookEventDTO)
+
         data = request_data.get("data", {})
         transaction_ref = data.get("reference", "")
         status = data.get("status", "")
         gateway_ref = data.get("customer", "").get("customer_code", "")
-        amount = data.get("amount", 0.0)//100  # Convert to Naira
+        amount = data.get("amount", 0.0) // 100  # Convert to Naira
         return GatewayWebhookEventDTO(
             internal_transaction_ref=transaction_ref,
             gateway_ref=gateway_ref,
             new_status=status,
-            amount=amount
+            amount=amount,
         )
 
-    def verify_payment(self, transaction_ref:str):
+    def verify_payment(self, transaction_ref: str):
         pass
-    
+
 
 class FlutterWaveAdapter(PaymentGatewayInterface):
     """
@@ -127,14 +175,23 @@ class FlutterWaveAdapter(PaymentGatewayInterface):
     - Handles webhook notifications specific to FlutterWave's format.
     - Verifies transactions using FlutterWave's verification API.
     """
-    
+
     headers = {
-    "accept": "application/json",
-    "Authorization": f"Bearer {settings.FLUTTERWAVE_SECRET_KEY}",
-    "Content-Type": "application/json"
+        "accept": "application/json",
+        "Authorization": f"Bearer {settings.FLUTTERWAVE_SECRET_KEY}",
+        "Content-Type": "application/json",
     }
-    
-    def process_payment(self, payment_details: PaymentDetails) -> GatewayProcessPaymentResponseDTO:
+
+    def process_payment(
+        self, payment_details: PaymentDetails
+    ) -> GatewayProcessPaymentResponseDTO:
+        """
+        Processes a payment using FlutterWave's API.
+        This method takes payment details, constructs a request to FlutterWave's API,
+        and returns a DTO with the result of the payment attempt.
+        :param payment_details: PaymentDetails object containing all necessary information for the payment.
+        :return: GatewayProcessPaymentResponseDTO containing the success status, gateway reference, and raw response data.
+        """
         endpoint = settings.FLUTTERWAVE_BANK_TRANSFER_ENDPOINT
         payload = {
             "amount": payment_details.amount,
@@ -142,7 +199,7 @@ class FlutterWaveAdapter(PaymentGatewayInterface):
             "currency": payment_details.currency,
             "tx_ref": payment_details.tx_ref,
             "full_name": payment_details.client_name,
-            "is_permanent": payment_details.is_permanent
+            "is_permanent": payment_details.is_permanent,
         }
         response = requests.post(endpoint, json=payload, headers=self.headers)
         response.raise_for_status()
@@ -155,31 +212,44 @@ class FlutterWaveAdapter(PaymentGatewayInterface):
             gateway_ref = authorization.get("transfer_reference")
 
         return GatewayProcessPaymentResponseDTO(
-            success=success,
-            gateway_ref=gateway_ref,
-            raw_response=data
+            success=success, gateway_ref=gateway_ref, raw_response=data
         )
-    
+
     def handle_webhook(self, request_data) -> GatewayWebhookEventDTO:
-        # Handle webhook notifications from FlutterWave
-        # simply for extracting data and passing to a dto
+        """
+        Handles webhook notifications from FlutterWave.
+        This method processes the incoming webhook data, extracts relevant information,
+        and returns a DTO with the transaction details.
+        :param request_data: Raw data from the webhook notification.
+        :return: GatewayWebhookEventDTO containing the internal transaction reference, gateway reference, new status, and amount.
+        """
+
         data = request_data.get("data", {})
         transaction_ref = data.get("tx_ref", "")
         status = data.get("status", "")
         gateway_ref = data.get("flw_ref", "")
         amount = data.get("amount", 0.0)
+        logger.info(
+            f"Handling webhook: tx_ref={transaction_ref}, status={status}, gateway_ref={gateway_ref}, amount={amount}"
+        )
         return GatewayWebhookEventDTO(
             internal_transaction_ref=transaction_ref,
             gateway_ref=gateway_ref,
             new_status=status,
-            amount=amount
+            amount=amount,
         )
-    
+
     def verify_payment(self, transaction_ref: str) -> dict:
-        # Verify payment using FlutterWave's verification API
-        endpoint = f"https://api.flutterwave.com/v3/charges?tx_ref={transaction_ref}"
+        """
+        Verifies a payment using FlutterWave's verification API.
+        This method constructs a request to FlutterWave's verification endpoint
+        and returns the verification result.
+        :param transaction_ref: The transaction reference to verify.
+        :return: A dictionary containing the verification result.
+        """
+        endpoint = settings.FLUTTERWAVE_VERIFICATION_URL.format(
+            transaction_ref=transaction_ref
+        )
         response = requests.get(endpoint, headers=self.headers)
         response.raise_for_status()
         return response.json()
-
-        
