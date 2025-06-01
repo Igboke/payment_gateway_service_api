b74c9fdc69ee0207cc46b4d4c7f489c302ff5e0e
Igboke Daniel <danieligboke669@gmail.com>
2025-06-01 12:12:39 +0100

    Refactor OrdersAdminForm to improve logging and string formatting consistency

--- payment_gateway_service_api/Orders/forms.py
+++ payment_gateway_service_api/Orders/forms.py
@@ -1,13 +1,20 @@
 from django import forms
 from .models import Orders
+import logging
+
+logger = logging.getLogger(__name__)
+
 
 class OrdersAdminForm(forms.ModelForm):
     class Meta:
         model = Orders
-        fields = '__all__'
+        fields = "__all__"
 
     def clean_status(self):
-        status = self.cleaned_data['status']
-        if status == 'failed' and not self.instance.pk:
-            raise forms.ValidationError("An order cannot be marked as failed upon creation.")
+        status = self.cleaned_data["status"]
+        if status == "failed" and not self.instance.pk:
+            logger.error("Attempt to mark a new order as failed.")
+            raise forms.ValidationError(
+                "An order cannot be marked as failed upon creation."
+            )
         return status
