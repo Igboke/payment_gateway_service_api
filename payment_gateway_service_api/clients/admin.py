4c7fe35a58fd949f43c597cf329b9d4b695630ce
Igboke Daniel <danieligboke669@gmail.com>
2025-06-01 12:08:59 +0100

    Refactor admin.py for improved readability and consistency in fieldsets and display options

--- payment_gateway_service_api/clients/admin.py
+++ payment_gateway_service_api/clients/admin.py
@@ -4,63 +4,89 @@ from django.contrib.auth.admin import UserAdmin
 from .forms import ClientCreationForm, ClientChangeForm, AddressCreationForm
 from .utils import Address
 
+
 @admin.register(Address)
 class AddressAdmin(admin.ModelAdmin):
     add_form = AddressCreationForm
-    list_display = ("street_line1","street_line2","city","state_province","country")
-    search_fields = ("city","state_province","country")
-    ordering = ("city",)  
+    list_display = ("street_line1", "street_line2", "city", "state_province", "country")
+    search_fields = ("city", "state_province", "country")
+    ordering = ("city",)
     filter_horizontal = ()
     add_fieldsets = (
-        (None, {
-            "classes":("wide","collapse"),
-            "fields":("street_line1","street_line2","city","state_province","country","postal_code"),    
-        }),
+        (
+            None,
+            {
+                "classes": ("wide", "collapse"),
+                "fields": (
+                    "street_line1",
+                    "street_line2",
+                    "city",
+                    "state_province",
+                    "country",
+                    "postal_code",
+                ),
+            },
+        ),
     )
+
 
 class ClientAdmin(UserAdmin):
     """
     This is the admin class for Client
     """
+
     form = ClientChangeForm
     add_form = ClientCreationForm
-    list_display = ("email","first_name","last_name","is_staff","is_active")
-    list_filter = ("is_staff","is_active")
-    search_fields = ("email","first_name","last_name","middle_name")
+    list_display = ("email", "first_name", "last_name", "is_staff", "is_active")
+    list_filter = ("is_staff", "is_active")
+    search_fields = ("email", "first_name", "last_name", "middle_name")
     ordering = ("email",)
     filter_horizontal = ()
     add_fieldsets = (
-        (None, {
-            "classes":("wide","collapse"),
-            "fields":("email","password","password2"),    
-        }),
-        ("Personal Info", {
-            "classes":("collapse",),
-            "fields":("first_name","middle_name","last_name")
-        }),
-        ("Address", {
-            "classes":("collapse",),
-            "fields":("house_address",)
-        }),
+        (
+            None,
+            {
+                "classes": ("wide", "collapse"),
+                "fields": ("email", "password", "password2"),
+            },
+        ),
+        (
+            "Personal Info",
+            {
+                "classes": ("collapse",),
+                "fields": ("first_name", "middle_name", "last_name"),
+            },
+        ),
+        ("Address", {"classes": ("collapse",), "fields": ("house_address",)}),
     )
     fieldsets = (
-        (None, {
-            "fields":("email",),
-        }),
-        ("Personal Info", {
-            "classes":("collapse",),
-            "fields":("first_name","middle_name","last_name")
-        }),
-        ("Address", {
-            "classes":("collapse",),
-            "fields":("house_address",)
-        }),
-        ("Permissions", {
-            "fields":("is_staff","is_active"),
-        }),
-        ("Important Dates", {
-            "fields":("last_login","date_joined"),
-        }),
+        (
+            None,
+            {
+                "fields": ("email",),
+            },
+        ),
+        (
+            "Personal Info",
+            {
+                "classes": ("collapse",),
+                "fields": ("first_name", "middle_name", "last_name"),
+            },
+        ),
+        ("Address", {"classes": ("collapse",), "fields": ("house_address",)}),
+        (
+            "Permissions",
+            {
+                "fields": ("is_staff", "is_active"),
+            },
+        ),
+        (
+            "Important Dates",
+            {
+                "fields": ("last_login", "date_joined"),
+            },
+        ),
     )
+
 
 admin.site.register(Client, ClientAdmin)
