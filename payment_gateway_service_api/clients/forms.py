bdb305c2583987de7f95ce82ef17ab5f199b87f7
Igboke Daniel <danieligboke669@gmail.com>
2025-06-01 12:09:13 +0100

    Refactor forms in payment_gateway_service_api for improved readability and consistency

--- payment_gateway_service_api/clients/forms.py
+++ payment_gateway_service_api/clients/forms.py
@@ -7,20 +7,25 @@ from .utils import Address
 
 Client = get_user_model()
 
+
 class AddressCreationForm(ModelForm):
     class Meta:
         model = Address
         fields = "__all__"
 
-# two forms user creation form and user update form
+
 class ClientCreationForm(ModelForm):
     """
     This is the user creation Form
     """
-    password = forms.CharField(label="Password",widget=forms.PasswordInput)
-    password2 = forms.CharField(label="Password Confirmation",widget=forms.PasswordInput)
+
+    password = forms.CharField(label="Password", widget=forms.PasswordInput)
+    password2 = forms.CharField(
+        label="Password Confirmation", widget=forms.PasswordInput
+    )
+
     class Meta:
-        model=Client
+        model = Client
         fields = "__all__"
 
     def clean_password2(self):
@@ -29,28 +34,29 @@ class ClientCreationForm(ModelForm):
 
         if password and password2 and password != password2:
             raise ValidationError("Password do not Match")
-        
+
         return password2
-    
-    def save(self, commit = True):
+
+    def save(self, commit=True):
         """
         This is a Model Form, hence we have to override the save method to set password
         """
-        #Create client instance but do not save
+        # Create client instance but do not save
         client = super().save(commit=False)
         client.set_password(self.cleaned_data["password"])
-        
+
         if commit:
             client.save()
             self.save_m2m()
-        
+
         return client
+
 
 class ClientChangeForm(UserChangeForm):
     """
     Client Change form inherits from User Change Form
     """
+
     class Meta:
         model = Client
         fields = "__all__"
-
