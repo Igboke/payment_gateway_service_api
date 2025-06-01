732020f10f9b794d442dcaffa677e60460cac5f5
Igboke Daniel <danieligboke669@gmail.com>
2025-06-01 11:35:01 +0100

    Refactor ProductAdmin for improved code readability and consistency in admin.py

--- payment_gateway_service_api/Products/admin.py
+++ payment_gateway_service_api/Products/admin.py
@@ -2,19 +2,23 @@ from django.contrib import admin
 from .forms import AddProductForm
 from .models import Products
 
+
 class ProductAdmin(admin.ModelAdmin):
     add_form = AddProductForm
-    list_display = ("name","quantity","price","is_available")
+    list_display = ("name", "quantity", "price", "is_available")
     list_filter = ("is_available",)
-    search_fields = ("name","price")
-    ordering = ("name",)  
+    search_fields = ("name", "price")
+    ordering = ("name",)
     filter_horizontal = ()
     add_fieldsets = (
-        (None, {
-            "classes":("wide","collapse"),
-            "fields":("name","quantity","description","price","is_available"),    
-        }),
+        (
+            None,
+            {
+                "classes": ("wide", "collapse"),
+                "fields": ("name", "quantity", "description", "price", "is_available"),
+            },
+        ),
     )
-    
 
-admin.site.register(Products,ProductAdmin)
+
+admin.site.register(Products, ProductAdmin)
