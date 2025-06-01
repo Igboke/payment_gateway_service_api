6643ac1479a14ca65caa51b35cb4a58310ea7ba7
Igboke Daniel <danieligboke669@gmail.com>
2025-06-01 12:12:25 +0100

    Refactor OrdersAdmin in admin.py for improved readability and consistency

--- payment_gateway_service_api/Orders/admin.py
+++ payment_gateway_service_api/Orders/admin.py
@@ -1,32 +1,49 @@
 from django.contrib import admin
-from django.forms import forms 
 from .models import Orders, OrderItem
+
 
 # Inline for OrderItem
 class OrderItemInline(admin.TabularInline):
     model = OrderItem
     extra = 1
 
+
 class OrdersAdmin(admin.ModelAdmin):
     inlines = [OrderItemInline]
 
-    list_display = ("client", "status", "total_amount", "shipping_address", "billing_address","created_at")
-    list_filter = ("status","created_at","client")
+    list_display = (
+        "client",
+        "status",
+        "total_amount",
+        "shipping_address",
+        "billing_address",
+        "created_at",
+    )
+    list_filter = ("status", "created_at", "client")
     search_fields = ("client__first_name", "client__last_name", "status")
 
     readonly_fields = ("total_amount", "created_at", "updated_at")
 
     fieldsets = (
-        (None, {
-            "fields": ("client", "status", ("shipping_address", "billing_address")),
-        }),
-        ("Order Details", {
-            "fields": ("total_amount",),
-        }),
-        ("Timestamps", {
-            "fields": ("created_at", "updated_at"),
-            "classes": ("collapse",),
-        }),
+        (
+            None,
+            {
+                "fields": ("client", "status", ("shipping_address", "billing_address")),
+            },
+        ),
+        (
+            "Order Details",
+            {
+                "fields": ("total_amount",),
+            },
+        ),
+        (
+            "Timestamps",
+            {
+                "fields": ("created_at", "updated_at"),
+                "classes": ("collapse",),
+            },
+        ),
     )
 
     def save_formset(self, request, form, formset, change):
