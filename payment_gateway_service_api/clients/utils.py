9dde70816d05fdd6a654189ca33eb4ef69a6d818
Igboke Daniel <danieligboke669@gmail.com>
2025-06-01 12:09:52 +0100

    Refactor Address model in utils.py for improved readability and consistency

--- payment_gateway_service_api/clients/utils.py
+++ payment_gateway_service_api/clients/utils.py
@@ -1,9 +1,11 @@
 from django.db import models
 
+
 class Address(models.Model):
     """
     Address model for billing or shipping
     """
+
     street_line1 = models.CharField(max_length=255)
     street_line2 = models.CharField(max_length=255, blank=True, null=True)
     city = models.CharField(max_length=100)
